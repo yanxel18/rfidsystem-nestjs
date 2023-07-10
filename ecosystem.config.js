3bb59049f86776efc85b7c205ad917d4fa069f26
yanxel18 <yanxel18@gmail.com>
2023-07-10 09:46:02 +0900

    added pm2 script

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      script: 'dist/main.js',
+      name: 'RFID-NESTJS',
+      exp_backoff_restart_delay: 100,
+      watch: true,
+      instances: 1,
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 import cluster from 'cluster';
 import * as os from 'os';
+import compression from 'compression';
 import helmet from 'helmet';
+import { NestExpressApplication } from '@nestjs/platform-express';
 const cpus = os.cpus();
 
 async function bootstrap() {
@@ -25,16 +27,30 @@ async function bootstrap() {
 }
 
 async function initializeSystem() {
-  const app: INestApplication = await NestFactory.create(AppModule, {
-    cors: true,
-    abortOnError: false,
-  });
+  const app: INestApplication =
+    await NestFactory.create<NestExpressApplication>(AppModule, {
+      cors: true,
+      abortOnError: false,
+    });
+
   const appConfig = app.getHttpAdapter().getInstance();
-  appConfig.disable('x-powered-by');
+  appConfig.disable('x-powered-by', 'X-Powered-By');
   appConfig.set('etag', false);
   appConfig.set('json spaces', 2);
   const prismaService = app.get(PrismaService);
-  if (process.env.NODE_ENV === 'production') app.use(helmet());
+  app.use(compression());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(helmet());
+    app.use(helmet.noSniff());
+    app.use(helmet.xssFilter());
+    app.use(
+      helmet.hsts({
+        maxAge: 15552000,
+        includeSubDomains: true,
+      }),
+    );
+  }
 
   app.useGlobalPipes(
     new ValidationPipe({
